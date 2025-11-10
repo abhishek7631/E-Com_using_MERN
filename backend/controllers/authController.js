cf0202b8759516238300f722880eeb05536bcdc8
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-11 00:22:37 +0530

    create login route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.registerController = async (req, res) => {
   try {
@@ -53,6 +54,55 @@ exports.registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+exports.loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.send({
+        success: false,
+        message: "Required email or password",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user)
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found" });
+
+    const compare = await bcrypt.compare(password, user.password);
+
+    if (!compare)
+      return res
+        .status(404)
+        .send({ success: false, message: "Wrong password" });
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "User Login Successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error in Login",
       error,
     });
   }
