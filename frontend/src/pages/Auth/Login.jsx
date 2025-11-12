9eaf779033bddf2bf1d4eb721c7be14142e05354
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-12 16:32:33 +0530

    login form design and connected from backend

---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../../styles/AuthStyles.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
+        { email, password }
+      );
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  }
+
+  return (
+    <Layout title="Login - Ecommerce App">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
