28da8b9202d8f36d07a35b35047ba2751655bbde
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-13 15:58:43 +0530

    implement context for homePage

--- frontend/src/context/Auth.jsx
+++ frontend/src/context/Auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -7,6 +7,18 @@ const AuthProvider = ({ children }) => {
     user: null,
     token: "",
   });
+
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token,
+      });
+    }
+  }, []);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
