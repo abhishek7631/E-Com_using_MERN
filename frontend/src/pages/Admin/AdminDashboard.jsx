373d764933566b42045d704ad50291fbe93145f7
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-15 21:23:21 +0530

    create controller and  for admin route

---
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const AdminDashboard = () => {
+  return <Layout>This is Admin page</Layout>;
+};
+
+export default AdminDashboard;
