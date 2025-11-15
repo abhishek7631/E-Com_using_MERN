3b3c565d2e11f8e82f6e6af03655052e666cf12b
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-15 23:38:06 +0530

    create admin menu page on admin dashboard

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
 
 const AdminDashboard = () => {
   return (
     <Layout>
-      <h1>This is Admin page</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">content</div>
+        </div>
+      </div>
     </Layout>
   );
 };
