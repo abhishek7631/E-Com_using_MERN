7d21a33f72cf68e1fdcae2a6d0ec87bb0fbd75d6
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-19 22:17:29 +0530

    update create category in UI

--- frontend/src/pages/Admin/CreateCategory.jsx
+++ frontend/src/pages/Admin/CreateCategory.jsx
@@ -3,9 +3,32 @@ import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+
+  const [name, setName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/v1/category/create-category`,
+        { name }
+      );
+
+      if (res.data?.success) {
+        toast.success(`${res.data.name} is created`);
+        getAllCategory();
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in input form");
+    }
+  };
 
   const getAllCategory = async () => {
     try {
@@ -35,6 +58,13 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
             <div className="w-75">
               <table class="table">
                 <thead>
@@ -49,7 +79,12 @@ const CreateCategory = () => {
                       <>
                         <td key={c._id}>{c.name}</td>
                         <td>
-                          <button className="btn btn-primary">Edit </button>
+                          <button className="btn btn-primary ms-2">
+                            Edit{" "}
+                          </button>
+                          <button className="btn btn-danger ms-2">
+                            Delete{" "}
+                          </button>
                         </td>
                       </>
                     ))}
