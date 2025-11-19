972564955160fd6eca3331332af2c8559b8710ca
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-11-19 20:15:57 +0530

    implement create category in UI

--- frontend/src/pages/Admin/CreateCategory.jsx
+++ frontend/src/pages/Admin/CreateCategory.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getAllCategory = async () => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/v1/category/get-category`
+      );
+
+      if (res.data.success) {
+        setCategories(res.data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went ewrong getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid p-3">
@@ -11,7 +34,29 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="w-75">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {categories?.map((c) => (
+                      <>
+                        <td key={c._id}>{c.name}</td>
+                        <td>
+                          <button className="btn btn-primary">Edit </button>
+                        </td>
+                      </>
+                    ))}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
