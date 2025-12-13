6ca104adcfcc489404fc41358c0598130fc22d67
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-12-13 17:09:13 +0530

    implement UI for similar product

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ProductDetails = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
 
   //get single product
   const productDetails = async () => {
@@ -16,6 +19,7 @@ const ProductDetails = () => {
         }`
       );
       setProduct(res.data?.product);
+      getSimilarProducts(res.data?.product._id, res.data?.product.category._id);
     } catch (error) {
       console.log(error);
     }
@@ -24,6 +28,21 @@ const ProductDetails = () => {
   useEffect(() => {
     if (params?.slug) productDetails();
   }, [params?.slug]);
+
+  //get similar products
+
+  const getSimilarProducts = async (pid, cid) => {
+    try {
+      const res = await axios.get(
+        `${
+          import.meta.env.VITE_API_URL
+        }/api/v1/product/related-product/${pid}/${cid}`
+      );
+      setRelatedProducts(res.data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Layout>
@@ -43,12 +62,46 @@ const ProductDetails = () => {
             <h6>Name : {product.name}</h6>
             <h6>Description : {product.description}</h6>
             <h6>Price : ₹ {product.price}</h6>
-            <h6>Category : {product.category.name}</h6>
+            <h6>Category : {product.category?.name}</h6>
             <button className="btn btn-secondary ms-1">ADD TO CART</button>
           </div>
         </div>
-
-        <div className="row">similar product</div>
+        <hr />
+        <div className="row container">
+          <h5>Similar Products</h5>
+          {relatedProducts.length < 1 && (
+            <p className="text-center">No Similar Products Found</p>
+          )}
+          <div className="d-flex flex-wrap">
+            {relatedProducts?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`${
+                    import.meta.env.VITE_API_URL
+                  }/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">
+                    {p.description.substring(0, 30)}...
+                  </p>
+                  <p className="card-text"> ₹ {p.price}</p>
+                  <button
+                    className="btn btn-primary me-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  <button className="btn btn-secondary ms-1">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   );
