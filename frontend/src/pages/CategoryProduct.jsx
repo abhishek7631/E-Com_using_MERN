1cfce0dabad16303d4d22f3d03c228c54adfb201
abhishek7631 <abhishek.choudhary7631@gmail.com>
2025-12-14 09:52:54 +0530

    create category wise product page

--- frontend/src/pages/CategoryProduct.jsx
+++ frontend/src/pages/CategoryProduct.jsx
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CategoryProduct = () => {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  const navigate = useNavigate();
+
+  const params = useParams();
+
+  const getProductByCategory = async () => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/v1/product/product-category/${
+          params.slug
+        }`
+      );
+      setProducts(res.data?.products);
+      setCategory(res.data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (params?.slug) getProductByCategory();
+  }, [params?.slug]);
+
   return (
     <Layout>
-      <div className="container">
-        <h1>Category Name</h1>
+      <div className="container mt-3">
+        <h4 className="text-center">Category - {category?.name}</h4>
+        <h6 className="text-center">{products?.length} result found</h6>
+        <div className="row offset-1">
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`${
+                    import.meta.env.VITE_API_URL
+                  }/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">
+                    {p.description.substring(0, 30)}...
+                  </p>
+                  <p className="card-text"> ₹ {p.price}</p>
+                  <button
+                    className="btn btn-primary me-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  <button className="btn btn-secondary ms-1">
+                    ADD TO CART
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* <div className="m-2 p-3">
+            {products && products.length < total && (
+              <Button
+                className="btn btn-warning"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? "Loading..." : "Load More"}
+              </Button>
+            )}
+          </div> */}
+        </div>
       </div>
     </Layout>
   );
